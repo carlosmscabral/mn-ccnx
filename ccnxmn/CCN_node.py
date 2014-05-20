c2f60cce4c65057014f62b2e75fd5b68e0e21a1b
Phi-Ho <hohoangphi@gmail.com>
2014-05-20 02:23:47 -0400

    Fixing white spaces

--- ccnxmn/CCN_node.py
+++ ccnxmn/CCN_node.py
@@ -1,151 +1,148 @@
 from mininet.node import Node, Host, CPULimitedHost
 
 class CCNHost( Host ):
-  "CCNHost is a Host that always runs the ccnd daemon"
+    "CCNHost is a Host that always runs the ccnd daemon"
 
-  def __init__( self, name, **kwargs ):
-    Host.__init__( self, name, **kwargs )
+    def __init__( self, name, **kwargs ):
 
-    if not CCNHost.inited:
-        CCNHost.init()
 
-    self.cmd("export CCND_DEBUG=6")
-    self.cmd("export CCND_LOG=./log.{0}".format(self.name))
-#   print self.params['cache']
+        Host.__init__( self, name, **kwargs )
+        if not CCNHost.inited:
+            CCNHost.init()
 
-    if self.params['cache'] != None:
-      self.cmd("export CCND_CAP={0}".format(self.params['cache']))
+        self.cmd("export CCND_DEBUG=6")
+        self.cmd("export CCND_LOG=./log.{0}".format(self.name))
+#       print self.params['cache']
+        if self.params['cache'] != None:
+          self.cmd("export CCND_CAP={0}".format(self.params['cache']))
 
-    self.cmd("export CCN_LOCAL_SOCKNAME=/tmp/.sock.ccnx.{0}".format(self.name))
-    self.cmd("ccndstart")
-    self.peerList = {}
+        self.cmd("export CCN_LOCAL_SOCKNAME=/tmp/.sock.ccnx.{0}".format(self.name))
+        self.cmd("ccndstart")
+        self.peerList = {}
 
     def config( self, fib=None, app=None, cache=None, **params ):
-      r = Node.config( self, **params )
 
-      self.setParam( r, 'app', fib=fib )
-      self.setParam( r, 'fib', app=app)
-      self.setParam( r, 'cache', cache=cache )
+        r = Node.config( self, **params )
 
-      return r
+        self.setParam( r, 'app', fib=fib )
+        self.setParam( r, 'fib', app=app)
+        self.setParam( r, 'cache', cache=cache )
+
+        return r
 
     def configCCN(self):
-      self.buildPeerIP()
-      self.setFIB()
+
+        self.buildPeerIP()
+        self.setFIB()
 
     def buildPeerIP(self):
-      for iface in self.intfList():
-        link = iface.link
-        
-        if link:
-          node1, node2 = link.intf1.node, link.intf2.node
-          
-          if node1 == self:
-            self.peerList[node2.name] = link.intf2.node.IP(link.intf2)
-            
-          else:
-            self.peerList[node1.name] = link.intf1.node.IP(link.intf1)
+        for iface in self.intfList():
+            link = iface.link
+            if link:
+                node1, node2 = link.intf1.node, link.intf2.node
+                if node1 == self:
+                    self.peerList[node2.name] = link.intf2.node.IP(link.intf2)
+                else:
+                    self.peerList[node1.name] = link.intf1.node.IP(link.intf1)
 
 
     def setFIB(self):
-      for name in self.params['fib']:
-        if not name:
-          pass
-          
-        else:
-          self.insert_fib(name[0],self.peerList[name[1]])
+
+        for name in self.params['fib']:
+            if not name:
+                pass
+            else:
+                self.insert_fib(name[0],self.peerList[name[1]])
 
 
     def insert_fib(self, uri, host):
-      self.cmd('ccndc add {0} tcp {1}'.format(uri,host))
- #    self.cmd('ccndc add {0} udp {1}'.format(uri,host))
+        self.cmd('ccndc add {0} tcp {1}'.format(uri,host))
+ #      self.cmd('ccndc add {0} udp {1}'.format(uri,host))
 
     def terminate( self ):
-      "Stop node."
-      self.cmd('ccndstop')
-      self.cmd('killall -r zebra ospf')
-      Host.terminate(self)
+        "Stop node."
+        self.cmd('ccndstop')
+        self.cmd('killall -r zebra ospf')
+        Host.terminate(self)
 
     inited = False
 
+
     @classmethod
     def init( cls ):
-      "Initialization for CCNHost class"
-      cls.inited = True
+        "Initialization for CCNHost class"
+        cls.inited = True
 
 class CPULimitedCCNHost( CPULimitedHost ):
-  '''CPULimitedCCNHost is a Host that always runs the ccnd daemon and extends CPULimitedHost.
-     It should be used when one wants to limit the resources of CCN routers and hosts '''
+    '''CPULimitedCCNHost is a Host that always runs the ccnd daemon and extends CPULimitedHost.
+       It should be used when one wants to limit the resources of CCN routers and hosts '''
 
 
-  def __init__( self, name, sched='cfs', **kwargs ):
-    CPULimitedHost.__init__( self, name, sched, **kwargs )
+    def __init__( self, name, sched='cfs', **kwargs ):
 
-    if not CCNHost.inited:
-      CCNHost.init()
+        CPULimitedHost.__init__( self, name, sched, **kwargs )
+        if not CCNHost.inited:
+            CCNHost.init()
 
-    self.cmd("export CCND_DEBUG=6")
-    self.cmd("export CCND_LOG=./log.{0}".format(self.name))
+        self.cmd("export CCND_DEBUG=6")
+        self.cmd("export CCND_LOG=./log.{0}".format(self.name))
+        if self.params['cache'] != None:
+                self.cmd("export CCND_CAP={0}".format(self.params['cache']))
 
-    if self.params['cache'] != None:
-      self.cmd("export CCND_CAP={0}".format(self.params['cache']))
+        self.cmd("export CCN_LOCAL_SOCKNAME=/tmp/.sock.ccnx.{0}".format(self.name))
+        self.cmd("ccndstart")
+        self.peerList = {}
 
-    self.cmd("export CCN_LOCAL_SOCKNAME=/tmp/.sock.ccnx.{0}".format(self.name))
-    self.cmd("ccndstart")
-    self.peerList = {}
+    def config( self, fib=None, app=None, cpu=None, cores=None, cache=None, **params):
 
-  def config( self, fib=None, app=None, cpu=None, cores=None, cache=None, **params):
-    r = CPULimitedHost.config(self,cpu,cores, **params)
+        r = CPULimitedHost.config(self,cpu,cores, **params)
 
-    self.setParam( r, 'app', fib=fib )
-    self.setParam( r, 'fib', app=app)
-    self.setParam( r, 'cache', cache=cache)
+        self.setParam( r, 'app', fib=fib )
+        self.setParam( r, 'fib', app=app)
+        self.setParam( r, 'cache', cache=cache)
 
-    return r
+        return r
 
-  def configCCN(self):
-    self.buildPeerIP()
-    self.setFIB()
+    def configCCN(self):
 
+        self.buildPeerIP()
+        self.setFIB()
 
-  def buildPeerIP(self):
-    for iface in self.intfList():
-      link = iface.link
-      
-      if link:
-        node1, node2 = link.intf1.node, link.intf2.node
-        
-        if node1 == self:
-          self.peerList[node2.name] = link.intf2.node.IP(link.intf2)
-          
-        else:
-          self.peerList[node1.name] = link.intf1.node.IP(link.intf1)
+    def buildPeerIP(self):
+        for iface in self.intfList():
+            link = iface.link
+            if link:
+                node1, node2 = link.intf1.node, link.intf2.node
+                if node1 == self:
+                    self.peerList[node2.name] = link.intf2.node.IP(link.intf2)
+                else:
+                    self.peerList[node1.name] = link.intf1.node.IP(link.intf1)
 
 
-  def setFIB(self):
-    for name in self.params['fib']:
-      if not name:
-        pass
-        
-      else:
-        self.insert_fib(name[0],self.peerList[name[1]])
+    def setFIB(self):
+
+        for name in self.params['fib']:
+            if not name:
+                pass
+            else:
+                self.insert_fib(name[0],self.peerList[name[1]])
 
 
-  def insert_fib(self, uri, host):
-    self.cmd('ccndc add {0} tcp {1}'.format(uri,host))
-#   self.cmd('ccndc add {0} udp {1}'.format(uri,host))
+    def insert_fib(self, uri, host):
+        self.cmd('ccndc add {0} tcp {1}'.format(uri,host))
+#       self.cmd('ccndc add {0} udp {1}'.format(uri,host))
 
-  def terminate( self ):
-    "Stop node."
-    self.cmd('ccndstop')
-    self.cmd('killall -r zebra ospf')
-    Host.terminate(self)
+    def terminate( self ):
+        "Stop node."
+        self.cmd('ccndstop')
+        self.cmd('killall -r zebra ospf')
+        Host.terminate(self)
 
-  inited = False
+    inited = False
 
 
-  @classmethod
-  def init( cls ):
-    "Initialization for CCNHost class"
-    cls.inited = True
+    @classmethod
+    def init( cls ):
+        "Initialization for CCNHost class"
+        cls.inited = True
 
--- ccnxmn/conf_parser.py
+++ ccnxmn/conf_parser.py
@@ -8,7 +8,7 @@ class confCCNHost():
         self.uri_tuples = uri_tuples
         self.cpu = cpu
         self.cores = cores
-        self.cache = cache
+	self.cache = cache
 
     def __repr__(self):
         return 'Name: ' + self.name + ' App: ' + self.app + ' URIS: ' + str(self.uri_tuples) + ' CPU:' + str(self.cpu) + ' Cores:' +str(self.cores) + ' Cache: ' + str(self.cache)
@@ -44,17 +44,17 @@ def parse_hosts(conf_arq):
         uri_list=[]
         cpu = None
         cores = None
-        cache = None
+	cache = None
 
         for uri in uris:
             if re.match("cpu",uri):
                 cpu = float(uri.split('=')[1])
             elif re.match("cores",uri):
                 cores = uri.split('=')[1]
-            elif re.match("cache",uri):
-                cache = uri.split('=')[1]
-            elif re.match("mem",uri):
-                mem = uri.split('=')[1]
+	    elif re.match("cache",uri):
+		cache = uri.split('=')[1]
+	    elif re.match("mem",uri):
+		mem = uri.split('=')[1]
             else:
                 uri_list.append((uri.split(',')[0],uri.split(',')[1]))
 
